ce4626d4b501caff87520afb7fc1c658a9d4c857
Alberto Castaño Burgos <alberto.casb@gmail.com>
2017-05-02 01:02:44 +0200

    Pushing things while Unreal keeps running

--- Source/Grabber/PositionReport.h
+++ Source/Grabber/PositionReport.h
@@ -23,6 +23,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+private:
+    UPROPERTY(EditAnywhere)
+    float testValue;
 };

c398da0a21da0d49b6edcfd4bbc791ab54814e72
Alberto Castaño Burgos <alberto.casb@gmail.com>
2017-05-01 23:02:23 +0200

    Using checkout -- from branch iss1 to master

---
+++ Source/Grabber/GrabberGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GrabberGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRABBER_API AGrabberGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

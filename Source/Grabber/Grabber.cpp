f584f2d559d4306d755e93b1aa9d5364387c1cea
Alberto Castaño Burgos <alberto.casb@gmail.com>
2017-05-01 22:51:42 +0200

    cleaning unreal test

--- Source/Grabber/Grabber.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Grabber.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Grabber, "Grabber" );
--- Source/Grabber/GrabberGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Grabber.h"
-#include "GrabberGameModeBase.h"
-
-
-
-

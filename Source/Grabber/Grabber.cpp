43a2a6bb5642a44f060861b40cc7ebdb623ca170
Alberto Castaño Burgos <alberto.casb@gmail.com>
2017-05-01 22:48:43 +0200

    Unreal Engine 4.15.1 clean project

---
+++ Source/Grabber/Grabber.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Grabber, "Grabber" );
